24a4617dba6f25fd49ac09eb59e2ded6f9600ef0
JoniPahikainen <joni.pahikainen01@gmail.com>
2023-03-15 21:15:02 +0200

    simple api req and fetch

--- app/src/main/java/com/example/rest_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/rest_kotlin/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.rest_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize TextView
+        val textView = findViewById<TextView>(R.id.textView1)
+        val refreshButton = findViewById<Button>(R.id.button1)
+
+        // Create OkHttp client
+        val client = OkHttpClient()
+
+        // Create request object
+        val request = Request.Builder()
+            .url("https://random-word-api.herokuapp.com/word")
+            .build()
+
+        // Make request asynchronously
+        fun makeRequest() {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    // Handle request failure
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    // Handle request success
+                    val responseData =
+                        response.body?.string()?.replace("[\"", "")?.replace("\"]", "")
+                    runOnUiThread {
+                        textView.text = responseData
+                    }
+                }
+            })
+        }
+
+        //Make initial request
+        makeRequest()
+
+        //Set OnClickListener
+        refreshButton.setOnClickListener {
+            makeRequest()
+        }
     }
 }
